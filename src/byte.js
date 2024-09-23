d7017b54d900df5d9bd4a5d6b9a879651d4781c8
handoko <srabutdotcom@gmail.com>
2024-09-24 06:44:40 +0700

    ver 0.3.2

--- src/byte.js
+++ src/byte.js
@@ -9,20 +9,75 @@ import * as set from "./set.js";
 export class Byte {
    static get = {
       BE: {
+         /**
+          * @method
+          * @param {Uint8Array} data 
+          * @param {number} pos - positive integer, represent byteoffset. 
+          * @return {number} Positive integer value
+          */
          b8: get.getUint8,
-         b16 : get.getUint16,
+         /**
+          * @method
+          * @param {Uint8Array} data 
+          * @param {number} pos - positive integer, represent byteoffset. 
+          * @return {number} Positive integer value
+          */
+         b16: get.getUint16,
+         /**
+          * @method
+          * @param {Uint8Array} data 
+          * @param {number} pos - positive integer, represent byteoffset. 
+          * @return {number} Positive integer value
+          */
          b24: get.getUint24,
+         /**
+          * @method
+          * @param {Uint8Array} data 
+          * @param {number} pos - positive integer, represent byteoffset. 
+          * @return {number} Positive integer value
+          */
          b32: get.getUint32,
       }
    }
    static set = {
       BE: {
+         /**
+          * convert an integer value to Uint8Array
+          * @param {number|string} int 
+          * @param {1|2|3|4|"1"|"2"|"3"|"4"} bytes 
+          * @return {Uint8Array}
+          */
          b8: set.Uint8BE,
+         /**
+          * convert an integer value to Uint8Array
+          * @param {number|string} int 
+          * @param {1|2|3|4|"1"|"2"|"3"|"4"} bytes 
+          * @return {Uint8Array}
+          */
          b16: set.Uint16BE,
+         /**
+          * convert an integer value to Uint8Array
+          * @param {number|string} int 
+          * @param {1|2|3|4|"1"|"2"|"3"|"4"} bytes 
+          * @return {Uint8Array}
+          */
          b24: set.Uint24BE,
+         /**
+          * convert an integer value to Uint8Array
+          * @param {number|string} int 
+          * @param {1|2|3|4|"1"|"2"|"3"|"4"} bytes 
+          * @return {Uint8Array}
+          */
          b32: set.Uint32BE,
       }
    }
+   /** 
+    * Concate two or more Uint8Array to one Uint8Array
+    * @param  {Uint8Array[]} uint8s [Uint8Array]
+    * @return {Uint8Array} Uint8Array
+    */
    static concat = concat
 }
+
+//npx -p typescript tsc ./src/byte.js --declaration --allowJs --emitDeclarationOnly --lib ESNext --outDir ./dist
 
--- type/byte.d.ts
+++ type/byte.d.ts
@@ -4,20 +4,73 @@
 export class Byte {
     static get: {
         BE: {
+            /**
+             * @method
+             * @param {Uint8Array} data
+             * @param {number} pos - positive integer, represent byteoffset.
+             * @return {number} Positive integer value
+             */
             b8: typeof get.getUint8;
+            /**
+             * @method
+             * @param {Uint8Array} data
+             * @param {number} pos - positive integer, represent byteoffset.
+             * @return {number} Positive integer value
+             */
             b16: typeof get.getUint16;
+            /**
+             * @method
+             * @param {Uint8Array} data
+             * @param {number} pos - positive integer, represent byteoffset.
+             * @return {number} Positive integer value
+             */
             b24: typeof get.getUint24;
+            /**
+             * @method
+             * @param {Uint8Array} data
+             * @param {number} pos - positive integer, represent byteoffset.
+             * @return {number} Positive integer value
+             */
             b32: typeof get.getUint32;
         };
     };
     static set: {
         BE: {
+            /**
+             * convert an integer value to Uint8Array
+             * @param {number|string} int
+             * @param {1|2|3|4|"1"|"2"|"3"|"4"} bytes
+             * @return {Uint8Array}
+             */
             b8: typeof set.Uint8BE;
+            /**
+             * convert an integer value to Uint8Array
+             * @param {number|string} int
+             * @param {1|2|3|4|"1"|"2"|"3"|"4"} bytes
+             * @return {Uint8Array}
+             */
             b16: typeof set.Uint16BE;
+            /**
+             * convert an integer value to Uint8Array
+             * @param {number|string} int
+             * @param {1|2|3|4|"1"|"2"|"3"|"4"} bytes
+             * @return {Uint8Array}
+             */
             b24: typeof set.Uint24BE;
+            /**
+             * convert an integer value to Uint8Array
+             * @param {number|string} int
+             * @param {1|2|3|4|"1"|"2"|"3"|"4"} bytes
+             * @return {Uint8Array}
+             */
             b32: typeof set.Uint32BE;
         };
     };
+    /**
+     * Concate two or more Uint8Array to one Uint8Array
+     * @param  {Uint8Array[]} uint8s [Uint8Array]
+     * @return {Uint8Array} Uint8Array
+     */
     static concat: typeof concat;
 }
 import * as get from "../src/get.js";
